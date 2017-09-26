d92f5d98a73eb9770099aa31f57b99e76f253d4a
Lau <YueCheung, Lau@TaiHing.FoTan>
2017-09-26 16:33:31 +0800

    add bootstrap add basic template

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,14 +1,16 @@
 <?php
 class Pages extends CI_Controller{
 	public function view($page='home'){
-		/*
+		$this->load->helper('url');
+		$this->load->helper('html');
+		
 		if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
         {
 				echo "SSSS";
                 // Whoops, we don't have a page for that!
                 show_404();
         }
-		*/
+
         $data['title'] = ucfirst($page); // Capitalize the first letter
 
         $this->load->view('templates/header', $data);
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,2 @@
-s
+
+<button class="btn btn-success">Testing</button>
