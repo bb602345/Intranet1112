44322da0eda623207d83c92db4f6431bddb6dd44
Lau <YueCheung, Lau@TaiHing.FoTan>
2017-09-29 09:44:11 +0800

    add style

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,2 +1,8 @@
+<div class="container">
 
-<button class="btn btn-success">Testing</button>
+  <div class="starter-template">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</div>
